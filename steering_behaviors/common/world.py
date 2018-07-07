0ef7b57fc6ac8b0dc7f1fb00f67ee0876062c748
bytezen <bytezen@email>
2018-07-07 13:00:54 -0700

    refactored GameWorld into its own class

---
+++ steering_behaviors/common/world.py
@@ -0,0 +1,191 @@
+import common.actors as Actors
+from common.actors import Vehicle, Crosshair, Obstacle
+from common.behavior import Behavior
+import common.params as Params
+
+import pygame as pg
+import pygame.gfxdraw
+from pygame.math import Vector2
+
+import random
+
+    
+
+class GameWorld:
+    def __init__(self,width,height):
+        #TODO: read this from config file
+
+        self._paused = False
+        self._window_dim = Vector2(width,height)
+        self._crosshair = Crosshair((self.width * 0.5,
+                                     self.height * 0.5))
+        
+##        self.display_params = Params.DisplayParams()
+        self.wander_params = Params.WanderParams()
+        self.obstacle_params = Params.ObstacleParams()
+        self.obstacles = []
+        self.agents = []
+
+        #display flags
+        self.view_keys = True
+        
+        self.show_walls = False
+        self.show_obstacles = True
+        self.show_path = False
+        self.show_wander_circle = False
+        self.show_steering_force = True
+        self.show_heading = False
+        self.show_feelers = False
+        self.show_detection_box = True
+        self.render_neighbors = False
+        self.show_tagged = True
+
+
+        self.show_cell_space_info = False
+        self.show_crosshair = False
+
+    def add_agent(self, agent):
+        self.agents.append(agent)
+        
+    def create_obstacles(self):
+        max_trys = 2000
+
+        #create a dictionary to store obstacles
+        #we will add obstacles to this dictionary if they do not collide with
+        #anything in the dictionary
+        
+        obstacleDict = {}
+        
+        def add_obstacle(o):
+            obstacleDict[(o.left,o.top,o.width,o.height)] = obstacle_rect(o).inflate_ip(20,20)
+
+        def obstacle_rect(obstacle):
+            return pg.Rect(obstacle.left,obstacle.top,obstacle.width,obstacle.height)
+        
+        #build dictionary from existing obstacles (should be empty initially)
+        for o in self.obstacles:
+            add_obstacles(o)
+        
+        for i in range(self.obstacle_params.number):
+            num_trys = 0
+            overlapped = True
+
+            #create an obstacle
+            tryme = Obstacle(pos = (random.randint(0.05*self.width ,0.95*self.width),
+                                    random.randint(0.05*self.height, 0.95*self.height))
+                             )
+##            tryme = Obstacle(pos = (0.25* WIDTH, 0.5* HEIGHT))            
+                            
+            while overlapped == True:
+                num_trys += 1
+                
+                # have we tried to many times?
+                if num_trys > max_trys: break
+
+                # do we intersect any existing obstacles
+                intersections = obstacle_rect(tryme).collidedict( obstacleDict )
+                
+                if intersections == None:
+                    self.obstacles.append(tryme)
+                    add_obstacle(tryme)
+                    break
+                
+    def tag_obstacles_in_view_range(self, actor, objs, search_range):
+        #TODO: use cell space partitioning for this
+        Actors.tag_neighbors(actor, objs , search_range)
+
+
+
+    def update(self, time_elapsed):
+        if self.paused:
+            return
+        
+        for a in self.agents:
+            a.update(time_elapsed)
+
+
+    def draw(self,surface):
+##        display = self.display_params
+
+        if self.show_walls:
+            for w in self.walls:
+                w.draw(surface)
+
+        if self.show_obstacles:
+            for o in self.obstacles:
+                o.draw()
+
+                if self.show_tagged and o.tagged:
+                    pygame.gfxdraw.aacircle(surface,
+                                            int(o.exact_pos.x),
+                                            int(o.exact_pos.y),
+                                            int(o.bounding_radius),
+                                            (200,200,0)) 
+
+
+        # render the agents
+        for a in self.agents:
+            a.draw(surface)
+##            if a.on(Behavior.WANDER) and display.show_wander_config:
+##                grab wander params from steering and params file
+##                use pygame.gfx to draw to screen
+
+
+            if self.show_cell_space_info:
+                pass
+            
+                    
+        if self.show_crosshair:
+            self._crosshair.draw()
+
+
+    @property
+    def width(self): return self._window_dim.x
+
+    @property
+    def height(self): return self._window_dim.y
+    
+    @property
+    def agents(self):
+        return self._agents
+
+    @agents.setter
+    def agents(self,agents):
+        self._agents = agents
+        
+    @property
+    def crosshair(self):
+        return self._crosshair.pos
+    
+    @crosshair.setter
+    def crosshair(self,args):
+        self._crosshair.pos = Vector2(args)
+
+    @property
+    def paused(self):
+        return self._paused
+    
+    @paused.setter
+    def paused(self,flag):
+        self._paused = flag
+        
+    @property
+    def window_dim(self):
+        return self._window_dim
+
+    @property
+    def render_steering_force(self):
+        return self._show_steering_force
+
+    @property
+    def show_crosshair(self):
+        return self._show_crosshair
+
+    @show_crosshair.setter
+    def show_crosshair(self, val):
+        self._show_crosshair = val
+
+    def toggle_behavior(self):
+        for a in self.agents:
+            a.toggle_behavior(BEHAVIOR)
+
